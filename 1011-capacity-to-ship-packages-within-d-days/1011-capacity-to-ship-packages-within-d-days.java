6c412d8cda51114b70e77df9aceb9ae27c5cfc21
Jatin6396 <142729229+Jatin6396@users.noreply.github.com>
2025-09-26 20:20:47 +0530

    Time: 11 ms (76.84%), Space: 47.2 MB (58.7%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,46 +1,45 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-       int res=-1;
-       int start=MAX(weights);
-       int end=SUM(weights);
-       while(start<=end){
-           int mid=start+(end-start)/2;
-           if(isShipmentispossible(weights,mid,days)){
-               res=mid;
-               end=mid-1;
-           }
-           else{
-               start=mid+1;
-           }
-       }
-       return res;
+        int start=1;
+    
+        int end=0;
+        for(int i=0;i<weights.length;i++){
+            start=Math.max(start,weights[i]);
+            end+=weights[i];
+        }
+        int ans=-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(isShipment(weights,mid,days)){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+
+
+
+        }
+
+        return ans;
     }
-    public static boolean isShipmentispossible(int [] arr,int mid,int days){
+
+
+    public  boolean isShipment(int[] arr,int mid,int days){
         int currday=1;
-        int capacity=0;
+        int cap=0;
+
         for(int i=0;i<arr.length;i++){
-            capacity+=arr[i];
-            if(capacity>mid){
+            cap+=arr[i];
+
+            if(cap>mid){
                 currday++;
-                capacity=arr[i];
+                cap=arr[i];
             }
-            if(currday>days) return false;
-        }return true;
-    }
-    public static int MAX(int[] arr){
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(max<arr[i]){
-                max=arr[i];
-            }
+            
         }
-        return max;
-    }
-    public static int SUM(int[] arr){
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
-        }
-        return sum;
+        return currday<=days;
     }
 }
