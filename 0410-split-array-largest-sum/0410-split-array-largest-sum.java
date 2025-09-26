b1ce8401bec061319c4ac2bcb4d286abaa0c0fa6
Jatin6396 <142729229+Jatin6396@users.noreply.github.com>
2025-09-26 20:01:02 +0530

    Time: 1 ms (54.57%), Space: 41.4 MB (41.47%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int splitArray(int[] arr, int k) {
+        int n=arr.length;
+        
+        if(n<k){
+           return -1;
+       }
+        int start=0;
+        int end=0;
+        int ans=-1;
+        
+        
+        for(int i=0;i<arr.length;i++){
+            start=Math.max(start,arr[i]);
+            end+=arr[i];
+        }
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            if(isValid(arr,mid,k)){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return ans;
+        
+    }
+     public static boolean isValid(int[] arr,int mid,int k){
+        int cnt=1;
+        int page=0;
+        
+        for(int i=0;i<arr.length;i++){
+            page+=arr[i];
+            
+            if(page>mid) {
+                cnt++;
+                page=arr[i];
+            }
+            
+            if(cnt>k) return false;
+        }
+        return true;
+    }
+}
