e1d9c2ada33fb12487ccd5ead6a68736b0b94a7d
Hunter <142729229+Jatin6396@users.noreply.github.com>
2025-10-02 16:10:39 +0530

    0046-permutations

--- LeetCode/Medium/0046-permutations/0046-permutations.java
+++ LeetCode/Medium/0046-permutations/0046-permutations.java
@@ -1,30 +1,28 @@
 class Solution {
-    public static void helper(List<List<Integer>> res,int[] nums,int idx,List<Integer> ans){
-   if(idx==nums.length){
-//    for(int i=0;i<nums.length;i++){
-//     ans.add(nums[i]);
-//    }
-   res.add(new ArrayList<>(ans));
-  }
-
-    for(int i=idx;i<nums.length;i++){
-        swap(nums,i,idx);
-        ans.add(nums[idx]);
-        helper(res,nums,idx+1,ans);
-        ans.remove(ans.size()-1);
-        swap(nums,i,idx);
-    }
-
-    }
-    public static void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-    }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        List<Integer> ans= new ArrayList<>();
-        helper(res,nums,0,ans);
-        return res;
+        List<List<Integer>>ls =new ArrayList<>();
+        List<Integer> ans=new ArrayList<>();
+        boolean[] visited=new  boolean[nums.length];
+        helper(ls,ans,visited,nums,0);
+        return ls;
+    }
+
+    public static void helper(List<List<Integer>>ls,List<Integer> ans,boolean[] visited,int[] arr,int j){
+        if(j==arr.length){
+            ls.add(new ArrayList<>(ans));
+            return;
+
+        }
+
+        for(int i=0;i<visited.length;i++){
+            if(!visited[i]){
+                visited[i]=true;
+                ans.add(arr[i]);
+                helper(ls,ans,visited,arr,j+1);
+                visited[i]=false;
+                ans.remove(ans.size()-1);
+                
+            }
+        }
     }
 }
