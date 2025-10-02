2deb6f0717ec40a0c0cd4dcf458264de763bc14e
Hunter <142729229+Jatin6396@users.noreply.github.com>
2025-10-03 00:49:41 +0530

    0217-contains-duplicate

---
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs=new HashSet<Integer>();
+        for(Integer i:nums){
+            if(!hs.add(i)) return true;
+        }return false;
+    }
+}
