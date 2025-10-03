0fd5b8d69c0e6eee39da49871f7bd1b56988c271
Hunter <142729229+Jatin6396@users.noreply.github.com>
2025-10-03 20:56:19 +0530

    0033-search-in-rotated-sorted-array

---
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int i=0;
+        int j=arr.length-1;
+        int ans=-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                break;
+            }else if(arr[i]<=arr[mid]){
+                if(target>=arr[i]&& target<arr[mid]){
+                    j=mid-1;
+                }
+                else {
+                    i=mid+1;
+                }
+            }else{
+                if(target<=arr[j]&& target>arr[mid]){
+                    
+                    i=mid+1;
+                }
+                else {
+                    j=mid-1;
+                }
+
+            }
+
+        } return ans;
+    }
+}
