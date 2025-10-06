37295bbc09327c8b42c525123356d07fd536d638
Hunter <142729229+Jatin6396@users.noreply.github.com>
2025-10-06 11:42:22 +0530

    0072-edit-distance

--- LeetCode/Medium/0072-edit-distance/0072-edit-distance.java
+++ LeetCode/Medium/0072-edit-distance/0072-edit-distance.java
@@ -1,21 +1,24 @@
 class Solution {
 
-    public static int helper(int cnt,String word1, String word2){
+    public static int helper(String word1, String word2,int[][] dp){
         if(word1.length()==0 && word2.length()==0) return 0;
         if(word1.length()==0 && word2.length()!=0) return word2.length();
         if(word1.length()!=0 && word2.length()==0) return word1.length();
 
       char ch1=word1.charAt(0);
       char ch2=word2.charAt(0);
+      if(dp[word1.length()][word2.length()]!=-1){
+        return dp[word1.length()][word2.length()];
+      }
 
       if(ch1==ch2){
-        return helper(cnt,word1.substring(1),word2.substring(1));
+        return helper(word1.substring(1),word2.substring(1),dp);
       }
       else{
-        int in=helper(cnt+1,word1.substring(1),word2);
-        int de=helper(cnt+1,word1,word2.substring(1));
-        int re=helper(cnt+1,word1.substring(1),word2.substring(1));
-        return Math.min(in,Math.min(de,re))+1;
+        int in=helper(word1.substring(1),word2,dp);
+        int de=helper(word1,word2.substring(1),dp);
+        int re=helper(word1.substring(1),word2.substring(1),dp);
+        return dp[word1.length()][word2.length()]=Math.min(in,Math.min(de,re))+1;
       }
     }
     public int minDistance(String word1, String word2) {
@@ -23,13 +26,12 @@ class Solution {
       int n=word1.length();
       int m=word2.length();
 
-     // int[] dp=new int[n+1][m+1];
+     int[][] dp=new int[n+1][m+1];
 
-    //   for(int[] row:dp){
-    //     Arrays.fill(row,-1);
-    //   }
+     for(int[] row:dp)
+      Arrays.fill(row,-1);
 
-      int ans=helper(0,word1,word2);  
+      int ans=helper(word1,word2,dp);  
       System.out.println(ans);
       return ans;
     }
