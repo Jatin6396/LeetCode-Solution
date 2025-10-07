385aa14d0dee12b53537d2209b0db23e08e367cf
Hunter <142729229+Jatin6396@users.noreply.github.com>
2025-10-08 00:30:09 +0530

    0083-remove-duplicates-from-sorted-list

--- LeetCode/Easy/0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ LeetCode/Easy/0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -9,22 +9,34 @@
  * }
  */
 class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-
-        if(head==null ) return head;
-        ListNode curr=head;
+    public static ListNode reverse(ListNode head){
         ListNode prev=null;
-
-        while(curr.next!=null){
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
             prev=curr;
-            curr=curr.next;
-           if(prev.val==curr.val){
-            prev.next=curr.next;
-            curr=prev;
-           }
-           
+            curr=next;
         }
+        return prev;
+    }
+    public ListNode deleteDuplicates(ListNode head) {
+    LinkedHashSet<Integer> hs=new LinkedHashSet<>();
+     ListNode temp=head;
+     while(temp!=null){
+        hs.add(temp.val);
+        temp=temp.next;
+     }
+     temp=new ListNode(-1);
+     ListNode tail=temp;
 
-        return head;
+     for (int val : hs) {  
+    tail.next = new ListNode(val); 
+    tail = tail.next;             
+     }
+
+    // head=reverse(temp.next);
+     return temp.next;
+    
     }
 }
