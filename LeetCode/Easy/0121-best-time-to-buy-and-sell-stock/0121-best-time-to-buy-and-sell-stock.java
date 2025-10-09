7794671d8e6e1d8d116258f5936de7c0e02dd447
Hunter <142729229+Jatin6396@users.noreply.github.com>
2025-10-09 23:44:28 +0530

    0121-best-time-to-buy-and-sell-stock

---
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        int price=prices[0];
+        for(int i=1;i<prices.length;i++){
+            if(price>prices[i]){
+                price=prices[i];
+            }
+            max=Math.max(max,prices[i]-price);
+
+            
+        } return max;
+    }
+}
